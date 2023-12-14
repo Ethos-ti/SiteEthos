3973f1f4d75fa39b38de07b2a4d654c098ae674a
daphnemartinsba <daphne.antonio@unesp.br>
2023-12-14 16:22:16 -0300

    primeiro commit

--- dev-scripts/zip.sh
+++ dev-scripts/zip.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CDIR=$( pwd )
 cd $DIR/../themes
-zip -r ../zips/base-theme-slug.zip base-theme-slug -x "base-theme-slug/node_modules/*"
+zip -r ../zips/ethos-theme.zip ethos-theme -x "ethos-theme/node_modules/*"
