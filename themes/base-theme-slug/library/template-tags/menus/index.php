1dbad0da9ba8d326ce8b203b4604a6f1282c476e
Leonardo <leopiccionia@gmail.com>
2024-04-30 20:36:26 -0300

    Optimize social menu icons

--- themes/base-theme-slug/library/template-tags/menus/index.php
+++ themes/base-theme-slug/library/template-tags/menus/index.php
@@ -1,6 +1,6 @@
 <?php
 
-function the_social_networks_menu( $image = true ) {
+function the_social_networks_menu( $color = true ) {
     $menu_items = get_menu_by_position( 'social-networks' );
 
     if ( ! $menu_items ) {
@@ -9,37 +9,30 @@ function the_social_networks_menu( $image = true ) {
 
     $icons_directory = get_template_directory() . '/assets/images/social-networks/';
 
-    $fa_icons = [
-        'facebook'  => 'fa-facebook-f',
-        'twitter'   => 'fa-twitter',
-        'youtube'   => 'fa-youtube',
-        'instagram' => 'fa-instagram',
-        'whatsapp'  => 'fa-whatsapp'
-    ];
-
     echo '<div class="social-menu">';
 
         foreach ( $menu_items as $item ) {
-			$network_name = sanitize_title( $item->post_title );
+			$network_name = $item->post_title;
+			$network_slug = sanitize_title( $network_name );
 			$html = '';
 
-            if ( $image ) {
-				// Checks if the icon file exists
-				if ( ! file_exists( $icons_directory . $network_name . '.svg' ) ) {
+            if ( $color ) {
+				if ( ! file_exists( $icons_directory . $network_slug . '-color.svg' ) ) {
 					break;
 				}
 
-				$html = file_get_contents( $icons_directory . $network_name . '.svg' );
+				$html = file_get_contents( $icons_directory . $network_slug . '-color.svg' );
             } else {
-				// Checks if item exists on array $fa_icons
-				if ( isset( $fa_icons[$network_name] ) ) {
-					$html = '<i class="fab ' . $fa_icons[$network_name] . '"></i>';
+				if ( ! file_exists( $icons_directory . $network_slug . '.svg' ) ) {
+					break;
 				}
+
+				$html = file_get_contents( $icons_directory . $network_slug . '.svg' );
             }
 
 			if ( $html ) {
-				echo '<div class="social-icon icon-' . $network_name . '">';
-				echo '<a href="' . $item->url . '" target="_blank">' . $html . '</a>';
+				echo '<div class="social-icon icon-' . $network_slug . '">';
+				echo '<a href="' . $item->url . '" title="' . $network_name .'" target="_blank">' . $html . '</a>';
 				echo '</div>';
 			}
         }
