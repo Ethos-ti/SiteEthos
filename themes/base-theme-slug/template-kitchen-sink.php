098b03e8dce092bff37512b846d80103aca0ee46
Leonardo <leopiccionia@gmail.com>
2024-05-02 15:49:01 -0300

    Form fields

--- themes/base-theme-slug/template-kitchen-sink.php
+++ themes/base-theme-slug/template-kitchen-sink.php
@@ -5,7 +5,7 @@
 get_header();
 ?>
 
-<div class="container">
+<div class="container kitchen-sink">
     <h1>&lt;h1&gt; Título 1</h1>
     <h2>&lt;h2&gt; Título 2</h2>
     <h3>&lt;h3&gt; Título 3</h3>
@@ -34,6 +34,113 @@ get_header();
             <a href="https://hacklab.com.br">site por <strong>hacklab<span>/</span></strong></a>
         </div>
     </div>
+
+    <hr/>
+
+    <div class="stack stack--large">
+        <div class="stack">
+            <div class="kitchen-sink__stack-item">Item</div>
+            <div class="kitchen-sink__stack-item">Item</div>
+            <div class="kitchen-sink__stack-item">Item</div>
+        </div>
+
+        <div class="stack stack--small">
+            <div class="kitchen-sink__stack-item">Item</div>
+            <div class="kitchen-sink__stack-item">Item</div>
+            <div class="kitchen-sink__stack-item">Item</div>
+        </div>
+
+        <div class="stack stack--large">
+            <div class="kitchen-sink__stack-item">Item</div>
+            <div class="kitchen-sink__stack-item">Item</div>
+            <div class="kitchen-sink__stack-item">Item</div>
+        </div>
+    </div>
+
+    <hr/>
+
+    <label class="form-field">
+        <span class="form-field__label">Texto</span>
+        <input class="text-input" type="text">
+    </label>
+
+    <label class="form-field">
+        <span class="form-field__label">Select</span>
+        <select class="select">
+            <option>Opção 1</option>
+            <option>Opção 2</option>
+        </select>
+    </label>
+
+    <label class="form-field">
+        <span class="form-field__label">Checkbox 1</span>
+        <input class="checkbox" type="checkbox">
+    </label>
+
+    <label class="form-field">
+        <input class="checkbox" type="checkbox">
+        <span class="form-field__label">Checkbox 2</span>
+    </label>
+
+    <label class="form-field">
+        <span class="form-field__label">Radio 1</span>
+        <input class="radio" type="radio">
+    </label>
+
+    <label class="form-field">
+        <input class="radio" type="radio">
+        <span class="form-field__label">Radio 2</span>
+    </label>
+
+    <hr/>
+
+    <label class="form-field form-field--disabled">
+        <span class="form-field__label">Texto</span>
+        <input class="text-input text-input--disabled" type="text" disabled>
+    </label>
+
+    <label class="form-field form-field--disabled">
+        <span class="form-field__label">Select</span>
+        <select class="select select--disabled" disabled>
+            <option>Opção 1</option>
+            <option>Opção 2</option>
+        </select>
+    </label>
+
+    <label class="form-field form-field--disabled">
+        <span class="form-field__label">Checkbox</span>
+        <input class="checkbox checkbox--disabled" type="checkbox" disabled>
+    </label>
+
+    <label class="form-field form-field--disabled">
+        <span class="form-field__label">Radio</span>
+        <input class="radio radio--disabled" type="radio" disabled>
+    </label>
+
+    <hr/>
+
+    <label class="form-field form-field--error">
+        <span class="form-field__label">Texto</span>
+        <input class="text-input text-input--error" type="text">
+    </label>
+
+    <label class="form-field form-field--error">
+        <span class="form-field__label">Select</span>
+        <select class="select select--error">
+            <option>Opção 1</option>
+            <option>Opção 2</option>
+        </select>
+    </label>
+
+    <label class="form-field form-field--error">
+        <span class="form-field__label">Checkbox</span>
+        <input class="checkbox checkbox--error" type="checkbox">
+    </label>
+
+    <label class="form-field form-field--error">
+        <span class="form-field__label">Radio</span>
+        <input class="radio radio--disabled" type="radio">
+    </label>
 </div>
 
 <?php get_footer(); ?>
