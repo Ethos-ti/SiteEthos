424695dc70f8676f811c08d7be510b9a7cac5ab3
Leonardo <leopiccionia@gmail.com>
2024-05-02 12:50:20 -0300

    Buttons

---
+++ themes/base-theme-slug/template-kitchen-sink.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Inventário de componentes
+ */
+get_header();
+?>
+
+<div class="container">
+    <h1>&lt;h1&gt; Título 1</h1>
+    <h2>&lt;h2&gt; Título 2</h2>
+    <h3>&lt;h3&gt; Título 3</h3>
+    <h4>&lt;h4&gt; Título 4</h4>
+    <h5>&lt;h5&gt; Título 5</h5>
+    <h6>&lt;h6&gt; Título 6</h6>
+    <p>&lt;p&gt; Parágrafo</p>
+
+    <hr/>
+    <button type="button" class="button">Default</button>
+    <button type="button" class="button button--solid">Solid</button>
+    <button type="button" class="button button--outline">Outline</button>
+    <button type="button" class="button button--solid button--large">Large</button>
+    <button type="button" class="button button--solid button--small">Small</button>
+    <button type="button" class="button button--solid button--primary">Primary</button>
+    <button type="button" class="button button--solid button--secondary">Secondary</button>
+    <button type="button" class="button button--solid button--highlight">Secondary</button>
+    <button type="button" class="button button--outline button--warning">Warning</button>
+    <button type="button" class="button button--solid button--helper">Helper</button>
+</div>
+
+<?php get_footer(); ?>
