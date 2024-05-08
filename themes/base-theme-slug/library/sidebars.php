7c91056fbbb44b9768fdb9472a01a0c4311a18f7
Filipe de Souza <filipe.souza@hacklab.com.br>
2024-05-08 17:48:44 -0300

    apply new widget area for footer

--- themes/base-theme-slug/library/sidebars.php
+++ themes/base-theme-slug/library/sidebars.php
@@ -56,6 +56,15 @@ function widgets_areas() {
 		'after_title'   => '</h2>',
 	));
 
+	register_sidebar(array(
+		'name'          => 'Footer Widgets',
+		'id'            => 'footer_widgets',
+		'before_widget' => '<div class="footer-widgets">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	));
+
 }
 
 add_action('widgets_init', 'widgets_areas');
