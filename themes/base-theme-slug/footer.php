4e90f82420fd065412f61f25e8d830b12e2d1b50
Filipe de Souza <filipe.souza@hacklab.com.br>
2024-05-09 18:05:44 -0300

    check the footer to see if the sidebar exists

--- themes/base-theme-slug/footer.php
+++ themes/base-theme-slug/footer.php
@@ -1,11 +1,12 @@
 </div>
 <?php wp_reset_postdata() ?>
-
-<footer class="main-footer">
-    <div class="main-footer__widgets container">
-        <?php dynamic_sidebar('footer_widgets') ?>
-    </div>
-</footer>
+<?php if ( is_active_sidebar( 'footer_widgets' ) ):?>
+    <footer class="main-footer">
+        <div class="main-footer__widgets container">
+            <?php dynamic_sidebar('footer_widgets') ?>
+        </div>
+    </footer>
+<?php endif; ?>
 <?php wp_footer() ?>
 
 </body>
