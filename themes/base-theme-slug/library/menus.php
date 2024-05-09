527d793b6ffee837d7b01e041bfc7aa403c01ec0
Everaldo Matias <matiaseveraldo@gmail.com>
2024-05-09 14:54:06 -0300

    Refactor menus.php file

--- themes/base-theme-slug/library/menus.php
+++ themes/base-theme-slug/library/menus.php
@@ -2,34 +2,26 @@
 
 namespace BaseThemeName;
 
-function custom_menus() {
-    register_nav_menu('main-menu', __('Menu Principal', 'base-textdomain'));
-    /* register_nav_menu('footer-menu', __('Menu de Rodapé', 'base-textdomain')); */
-
-    register_nav_menu('social-networks', __('Redes Sociais', 'base-textdomain'));
-    register_nav_menu('footer-menu-first', __('Menu de Rodapé 1', 'base-textdomain'));
-    register_nav_menu('footer-menu-second', __('Menu de Rodapé 2', 'base-textdomain'));
-    register_nav_menu('footer-menu-third', __('Menu de Rodapé 3', 'base-textdomain'));
-    register_nav_menu('footer-menu-fourth', __('Menu de Rodapé 4', 'base-textdomain'));
+function register_menus() {
+    register_nav_menu( 'main-menu', __( 'Menu Principal', 'base-textdomain' ) );
+    register_nav_menu( 'social-networks', __( 'Redes Sociais', 'base-textdomain' ) );
 }
 
-add_action('init', 'BaseThemeName\\custom_menus');
+add_action( 'init', 'BaseThemeName\\register_menus' );
 
-
-// adiciona seta nos itens com subitens
-add_filter('walker_nav_menu_start_el', 'BaseThemeName\\add_arrow', 10, 4);
-function add_arrow($output, $item, $depth, $args) {
-
-    //Only add class to 'top level' items on the 'primary' menu.
-    if ($depth === 0) {
-        if (in_array("menu-item-has-children", $item->classes)) {
-            $output .= '<i class="fas fa-angle-down"></i>';
+function add_menu_arrow( $output, $item, $depth, $args ) {
+    // Only add class to 'top level' items on the 'primary' menu.
+    if ( $depth === 0 ) {
+        if ( in_array( 'menu-item-has-children', $item->classes ) ) {
+            $output .= '<i class="arrow--down"></i>';
         }
     }
     return $output;
 }
 
-//Descomente para adicionar css para top menu (WIP)
+add_filter( 'walker_nav_menu_start_el', 'BaseThemeName\\add_menu_arrow', 10, 4 );
+
+// Descomente para adicionar css para top menu (WIP)
 // add_filter('css_files_before_output','BaseThemeName\\add_top_menu', 10, 1);
 function add_top_menu($files){
     $files['top-menu'] = array(
