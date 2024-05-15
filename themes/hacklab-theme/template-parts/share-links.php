e302d7896202c29e8bb1b918097d020350c0b7b8
Filipe de Souza <filipe.souza@hacklab.com.br>
2024-05-15 16:57:07 -0300

    create share-links component and apply styles in post-header (#42)

--- themes/hacklab-theme/template-parts/share-links.php
+++ themes/hacklab-theme/template-parts/share-links.php
@@ -1,7 +1,12 @@
-<div class="post-content--subshare">
+<div class="share-links">
     <span><?php _e( 'Share', 'hacklabr' ) ?></span>
-    <a href="https://www.facebook.com/sharer/sharer.php?u=<?= get_the_permalink() ?>" target="_blank"><i class="fab fa-facebook-f"></i></a>
-    <a href="https://twitter.com/intent/tweet?text=<?= urlencode(get_the_title()) ?>&url=<?= get_the_permalink() ?>" target="_blank"><i class="fab fa-twitter"></i></a>
+    <a href="https://twitter.com/intent/tweet?text=<?= urlencode(get_the_title()) ?>&url=<?= get_the_permalink() ?>" target="_blank">
+        <iconify-icon icon="tabler:brand-x"></iconify-icon>
+    </a>
+    <a href="https://www.facebook.com/sharer/sharer.php?u=<?= get_the_permalink() ?>" target="_blank">
+        <iconify-icon icon="fa6-brands:facebook-f"></iconify-icon>
+    </a>
+
     <a href="whatsapp://send?text=<?= (get_the_title().' - '.get_the_permalink()) ?>" target="_blank" class="hide-for-large"><i class="fab fa-whatsapp"></i></a>
     <a href="https://api.whatsapp.com/send?text=<?= (get_the_title().' - '.get_the_permalink()) ?>" class="show-for-large" target="_blank"><i class="fab fa-whatsapp"></i></a>
     <a href="https://telegram.me/share/url?url=<?= get_the_title().' - '.get_the_permalink() ?>" target="_blank"><i class="fab fa-telegram"></i></a>
