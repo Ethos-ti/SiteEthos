88de66a6a26be9900c920cc08d57a95b9d188f49
Filipe de Souza <filipe.souza@hacklab.com.br>
2024-05-15 17:57:28 -0300

    change container size in post-header, apply styles and iconify in share-links (#42)

--- themes/hacklab-theme/template-parts/share-links.php
+++ themes/hacklab-theme/template-parts/share-links.php
@@ -1,13 +1,22 @@
 <div class="share-links">
     <span><?php _e( 'Share', 'hacklabr' ) ?></span>
-    <a href="https://twitter.com/intent/tweet?text=<?= urlencode(get_the_title()) ?>&url=<?= get_the_permalink() ?>" target="_blank">
-        <iconify-icon icon="tabler:brand-x"></iconify-icon>
-    </a>
-    <a href="https://www.facebook.com/sharer/sharer.php?u=<?= get_the_permalink() ?>" target="_blank">
-        <iconify-icon icon="fa6-brands:facebook-f"></iconify-icon>
-    </a>
 
-    <a href="whatsapp://send?text=<?= (get_the_title().' - '.get_the_permalink()) ?>" target="_blank" class="hide-for-large"><i class="fab fa-whatsapp"></i></a>
-    <a href="https://api.whatsapp.com/send?text=<?= (get_the_title().' - '.get_the_permalink()) ?>" class="show-for-large" target="_blank"><i class="fab fa-whatsapp"></i></a>
-    <a href="https://telegram.me/share/url?url=<?= get_the_title().' - '.get_the_permalink() ?>" target="_blank"><i class="fab fa-telegram"></i></a>
+    <div class="share-links__icons">
+        <a href="https://twitter.com/intent/tweet?text=<?= urlencode(get_the_title()) ?>&url=<?= get_the_permalink() ?>" target="_blank">
+            <iconify-icon icon="tabler:brand-x"></iconify-icon>
+        </a>
+        <a href="https://www.facebook.com/sharer/sharer.php?u=<?= get_the_permalink() ?>" target="_blank">
+            <iconify-icon icon="fa6-brands:facebook-f"></iconify-icon>
+        </a>
+    </div>
+
+    <a href="whatsapp://send?text=<?= (get_the_title().' - '.get_the_permalink()) ?>" target="_blank" class="hide-for-large">
+        <iconify-icon icon=""></iconify-icon>
+    </a>
+    <a href="https://api.whatsapp.com/send?text=<?= (get_the_title().' - '.get_the_permalink()) ?>" class="show-for-large" target="_blank">
+        <iconify-icon icon=""></iconify-icon>
+    </a>
+    <a href="https://telegram.me/share/url?url=<?= get_the_title().' - '.get_the_permalink() ?>" target="_blank">
+        <iconify-icon icon=""></iconify-icon>
+    </a>
 </div>
