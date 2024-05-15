9c87dc35f73cc4a75b9abd9cd6d5835809521427
Filipe de Souza <filipe.souza@hacklab.com.br>
2024-05-15 11:40:17 -0300

    initial refactoring single #38

--- themes/hacklab-theme/single.php
+++ themes/hacklab-theme/single.php
@@ -4,74 +4,22 @@
  */
 
 get_header();
+the_post();
+$category = get_the_category();
 ?>
 
-<div class="container single" id="single">
-
-    <div class="content" id="content">
-        <?php while ( have_posts() ) :
-			the_post();
-        ?>
-            <div class="post" id="post">
-
-                <div class="post-header">
-
-                    <div class="info">
-
-                        <span class="category"><?php the_category(', '); ?></span>
-
-                    </div>
-
-                    <div class="title">
-                        <h4> <?php the_title(); ?> </h4>
-                    </div>
-
-
-                    <div class="data">
-                        <?php the_date(); ?>
-                    </div>
-
-                </div>
-
-                <div class="post-content">
-                    <?php the_content(); ?>
-                </div>
-
-                <div class="post-footer">
-
-                    <div class="post-footer-tags">
-                        <?php echo get_html_terms( get_the_ID(), 'post_tag', true ); ?>
-                    </div>
-
-                    <div class="post-footer-share">
-                        <h6><?php _e( 'Share', 'hacklabr' ); ?></h6>
-
-                        <div class="social-media">
-
-                            <a href="https://www.facebook.com/sharer/sharer.php?u=<?= get_the_permalink() ?>" target="_blank"><i class="fab fa-facebook-square"></i></a>
-                            <a href="whatsapp://send?text=<?= (get_the_title().' - '.get_the_permalink()) ?>" target="_blank" class="hide-for-large"><i class="fab fa-whatsapp"></i></a>
-                            <a href="https://api.whatsapp.com/send?text=<?= (get_the_title().' - '.get_the_permalink()) ?>" class="show-for-large" target="_blank"><i class="fab fa-whatsapp"></i></a>
-                            <a href="https://telegram.me/share/url?url=<?= get_the_title().' - '.get_the_permalink() ?>" target="_blank"><i class="fab fa-telegram-plane"></i></a>
-
-                            <input type="text" style="display: none;" id="url" value="<?= get_the_permalink() ?>" />
-                            <a href="" class="copy" url="<?= get_the_permalink() ?>"> <i class="fas fa-link"></i> </a>
-                            <span id="alert" class="hide"> </span>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <?php get_template_part( 'template-parts/content/related-posts' ); ?>
-        <?php endwhile;?>
+<div class="container container--wide single">
+    <div class="featured-image">
+        <?= get_the_post_thumbnail(null, 'post-thumbnail',['class'=>'featured-image']); ?>
     </div>
 
-    <aside class="col-sm-3">
-        <?php dynamic_sidebar( 'sidebar-posts' ) ?>
-    </aside>
+    <div class="tags">
+        <a class="tag tag--category-<?= $category[0]->slug ?>" href="<?= get_term_link($category[0], 'category') ?>">
+            <?= $category[0]->name ?>
+        </a>
+    </div>
+
+    <h1 class="single--title"> <?php the_title(); ?> </h1>
 
 </div>
 
