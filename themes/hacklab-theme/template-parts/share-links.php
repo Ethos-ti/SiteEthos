f2a9f43ac7c9c9e5d261bddaf76c0dc6c313f77d
Leonardo <leopiccionia@gmail.com>
2024-05-20 17:07:55 -0300

    Add missing icons

--- themes/hacklab-theme/template-parts/share-links.php
+++ themes/hacklab-theme/template-parts/share-links.php
@@ -3,7 +3,7 @@
 
     <div class="share-links__icons">
         <a href="https://twitter.com/intent/tweet?text=<?= urlencode(get_the_title()) ?>&url=<?= get_the_permalink() ?>" target="_blank">
-            <iconify-icon icon="tabler:brand-x"></iconify-icon>
+            <iconify-icon icon="fa6-brands:x-twitter"></iconify-icon>
         </a>
         <a href="https://www.facebook.com/sharer/sharer.php?u=<?= get_the_permalink() ?>" target="_blank">
             <iconify-icon icon="fa6-brands:facebook-f"></iconify-icon>
@@ -11,12 +11,12 @@
     </div>
 
     <a href="whatsapp://send?text=<?= (get_the_title().' - '.get_the_permalink()) ?>" target="_blank" class="hide-for-large">
-        <iconify-icon icon=""></iconify-icon>
+        <iconify-icon icon="formkit:whatsapp"></iconify-icon>
     </a>
     <a href="https://api.whatsapp.com/send?text=<?= (get_the_title().' - '.get_the_permalink()) ?>" class="show-for-large" target="_blank">
-        <iconify-icon icon=""></iconify-icon>
+        <iconify-icon icon="formkit:whatsapp"></iconify-icon>
     </a>
     <a href="https://telegram.me/share/url?url=<?= get_the_title().' - '.get_the_permalink() ?>" target="_blank">
-        <iconify-icon icon=""></iconify-icon>
+        <iconify-icon icon="file-icons:telegram"></iconify-icon>
     </a>
 </div>
