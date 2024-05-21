a48335d2f8b0cb02dcfcbcb1a7c0c8cf07ba1c8c
Leonardo <leopiccionia@gmail.com>
2024-05-21 09:58:04 -0300

    Allow SVG uploads

--- themes/hacklab-theme/library/menus.php
+++ themes/hacklab-theme/library/menus.php
@@ -20,25 +20,3 @@ function add_menu_arrow( $output, $item, $depth, $args ) {
 }
 
 add_filter( 'walker_nav_menu_start_el', 'hacklabr\\add_menu_arrow', 10, 4 );
-
-// Descomente para adicionar css para top menu (WIP)
-// add_filter('css_files_before_output','hacklabr\\add_top_menu', 10, 1);
-function add_top_menu($files){
-    $files['top-menu'] = array(
-        'file' => '_c-top-menu.css',
-        'global' => true,
-        'inline' => true,
-    );
-    return $files;
-}
-// Descomente para alterar menu para sempre funcionar como hamburguer
-// add_filter('css_files_before_output','hacklabr\\sempre_hamburguer', 10, 1);
-function sempre_hamburguer($files){
-    $files['sempre-hamburguer'] = array(
-        'file' => 'menu-sempre-hamburguer.css',
-        'global' => true,
-        'inline' => true,
-    );
-    return $files;
-}
-
