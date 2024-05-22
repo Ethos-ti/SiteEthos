704ae9c19ca215c2f1b9d297f365fe8e9807095a
Thiago Monteiro <thiagomonteiro.geo@gmail.com>
2024-05-22 12:59:23 -0300

    aplica layout-parts nas archives

--- themes/hacklab-theme/archive.php
+++ themes/hacklab-theme/archive.php
@@ -1,30 +1,37 @@
 <?php
-get_header(); ?>
+get_header();
+
+global $wp_query;
+$post_type = get_queried_object()->name;
+
+?>
 
     <div class="container container--wide">
 
-            <?php get_template_part( 'template-parts/title/archive-header' ); ?>
+        <?php
+        if ( $post_type === 'publicacao' ) :
+            echo hacklabr\get_layout_part( 'publicacoes', 'header' );
+        elseif( is_tax('tipo_post', 'opinioes-e-analises') ) :
+            echo hacklabr\get_layout_part( 'opiniao', 'header' );
+        elseif( is_tax('tipo_post', 'posicionamentos-institucionais') ) :
+            echo hacklabr\get_layout_part( 'posicionamentos-institucionais', 'header' );
+        // elseif( $post_type === 'tribe_events' ) :
+        //     echo get_layout_header( 'agenda' );
+        endif;
+        ?>
 
-            <div class="archive__infos">
-                <?php get_template_part( 'template-parts/filter', 'posts', ['taxonomy' => 'category'] ); ?>
-            </div><!-- .infos -->
+        <main class="archive__content col-md-12">
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php get_template_part( 'template-parts/post-card', 'vertical' ); ?>
+            <?php endwhile; ?>
+        </main>
 
-            <main class="archive__content col-md-9">
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <?php get_template_part( 'template-parts/post-card', 'vertical' ); ?>
-                <?php endwhile; ?>
-            </main>
+        <?php
+        the_posts_pagination([
+            'prev_text' => __( '<iconify-icon icon="iconamoon:arrow-left-2-bold"></iconify-icon>', 'hacklbr'),
+            'next_text' => __( '<iconify-icon icon="iconamoon:arrow-right-2-bold"></iconify-icon>', 'hacklbr'),
 
-            <?php
-            the_posts_pagination([
-                'prev_text' => __( '<iconify-icon icon="iconamoon:arrow-left-2-bold"></iconify-icon>', 'hacklbr'),
-                'next_text' => __( '<iconify-icon icon="iconamoon:arrow-right-2-bold"></iconify-icon>', 'hacklbr'),
-
-            ]); ?>
-
-            <aside class="archive__sidebar col-md-3">
-                <?php dynamic_sidebar( 'sidebar-default' ) ?>
-            </aside>
+        ]); ?>
 
     </div><!-- /.container -->
 
