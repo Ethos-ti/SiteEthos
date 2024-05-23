00c16fd5450e08f5d226871868468354c77d9d75
Everaldo Matias <matiaseveraldo@gmail.com>
2024-05-23 10:45:24 -0300

    Refactor front-page.php

--- themes/hacklab-theme/front-page.php
+++ themes/hacklab-theme/front-page.php
@@ -1,30 +1,9 @@
-<?php
-get_header();
-?>
+<?php get_header(); ?>
 
-<div class="index-wrapper">
-    <div class="container container--wide">
-        <div class="row">
-            <?php get_template_part( 'template-parts/title/blog' ); ?>
+<div class="container container--wide">
+    <div class="post-content content content--normal">
+        <?php the_content(); ?>
+    </div>
+</div>
 
-            <div class="infos">
-                <?php get_template_part( 'template-parts/filter', 'posts', ['taxonomy' => 'category'] ); ?>
-            </div><!-- .infos -->
-
-            <main class="">
-                <?php dynamic_sidebar('widget-parceiros') ?>
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <?php get_template_part( 'template-parts/content/post' ); ?>
-                <?php endwhile; ?>
-
-                <?php get_template_part( 'template-parts/content/pagination' ); ?>
-            </main>
-
-            <aside class="">
-                <?php dynamic_sidebar( 'sidebar-posts' ) ?>
-            </aside>
-        </div><!-- /.row -->
-    </div><!-- /.container -->
-</div><!-- /.index-wrapper -->
-
-<?php get_footer();
+<?php get_footer(); ?>
