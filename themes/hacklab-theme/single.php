3fd7c82b0f0cf844f5abaaad60fea1f056d376a6
Filipe de Souza <filipe.souza@hacklab.com.br>
2024-05-23 15:43:39 -0300

    correction string declaration (#13)

--- themes/hacklab-theme/single.php
+++ themes/hacklab-theme/single.php
@@ -28,12 +28,12 @@ $excerpt = !empty( $post->post_excerpt ) ? wp_kses_post( $post->post_excerpt ) :
         <?php endif; ?>
 
         <div class="post-header__meta container container--normal">
-            <p class="post-header__date"><?= _e('Publicado em: ') ?><?= get_the_date() ?></p>
+            <p class="post-header__date"><?php _e('Published by:', 'hacklabr') ?><?= get_the_date() ?></p>
             <?php get_template_part('template-parts/share-links', null, ['link'=>get_the_permalink()]) ?>
         </div>
 
         <div class="post-header__author container container--normal">
-            <p class="post-header__author-name"><?= _e('Publicado por: ') ?><?= get_the_author() ?></p>
+            <p class="post-header__author-name"><?php _e('Published by:', 'hacklabr') ?><?= get_the_author() ?></p>
         </div>
 
         <div class="post-content container container--normal">
