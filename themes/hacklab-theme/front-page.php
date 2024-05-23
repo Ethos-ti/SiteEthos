61ea8d580898e3881b4425acc50e092bb1215367
dioliviers <dioliviers@gmail.com>
2024-05-23 10:48:55 -0300

    created front-page

---
+++ themes/hacklab-theme/front-page.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<div class="container container--wide">
+    <div class="post-content content content--normal">
+        <?php the_content(); ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
