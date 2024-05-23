399c056e24354e48aae171a9e464467b4cf8a8b5
dioliviers <dioliviers@gmail.com>
2024-05-23 17:28:29 -0300

    ajustado css dos parceiros

--- themes/hacklab-theme/template-parts/partners-list.php
+++ themes/hacklab-theme/template-parts/partners-list.php
@@ -1,7 +1,7 @@
 
 
 <?php if ( ( is_single() || is_page() || is_home() ) && get_post_meta( get_the_ID(), '0', true ) ) : ?>
-    <div class="partners-list container">
+    <div class="partners-list container container--wide">
         <?php echo hacklabr\get_layout_footer( 'parceiros' ); ?>
     </div>
 <?php endif; ?>
