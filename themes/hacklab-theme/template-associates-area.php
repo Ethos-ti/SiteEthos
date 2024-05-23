0abfd7d68afedec9b55e7151e0b2bc080a0e8a98
Everaldo Matias <matiaseveraldo@gmail.com>
2024-05-23 16:58:46 -0300

    WIP: Refactor component content-sidebar #20

--- themes/hacklab-theme/template-associates-area.php
+++ themes/hacklab-theme/template-associates-area.php
@@ -10,27 +10,29 @@ $login_url = \function_exists( 'pmpro_url' ) && ( $pmpro_url = pmpro_url( 'login
 
 ?>
 
-<div class="content-sidebar container container--wide">
-    <aside class="content-sidebar__sidebar">
-        <p class="content-sidebar__sidebar--description"><?= __( 'Associeted panel', 'base-textdomain' ); ?></p>
-        <?php
-        $associates_areas = \get_pages_by_template( 'template-associates-area.php' );
+<div class="content-sidebar">
+    <div class="container container--wide content-sidebar__container">
+        <aside class="content-sidebar__sidebar">
+            <p class="content-sidebar__sidebar-description"><?= __( 'Associeted panel', 'base-textdomain' ); ?></p>
+            <?php
+            $associates_areas = \get_pages_by_template( 'template-associates-area.php' );
 
-        if ( $associates_areas ) {
-            echo '<ul class="content-sidebar__list">';
-            foreach ( $associates_areas as $associates_area ) {
-                $css_class = $current_post_id === $associates_area->ID ? 'content-sidebar__list-item active' : 'content-sidebar__list-item';
-                echo '<li class="' . $css_class . '"><a href="' . esc_url( get_permalink( $associates_area ) ) . '">' . wp_kses_post( get_the_title( $associates_area ) ) . '</a></li>';
+            if ( $associates_areas ) {
+                echo '<ul class="content-sidebar__list">';
+                foreach ( $associates_areas as $associates_area ) {
+                    $css_class = $current_post_id === $associates_area->ID ? 'content-sidebar__list-item content-sidebar__list-item--active' : 'content-sidebar__list-item';
+                    echo '<li class="' . $css_class . '"><a href="' . esc_url( get_permalink( $associates_area ) ) . '">' . wp_kses_post( get_the_title( $associates_area ) ) . '</a></li>';
+                }
+                echo '</ul>';
             }
-            echo '</ul>';
-        }
-        ?>
+            ?>
 
-        <a class="button button--outline" href="<?= wp_logout_url( $login_url ); ?>"><?= __( 'Logout', 'base-textdomain' ); ?></a>
-    </aside>
-    <main class="content-sidebar__content">
-        <?php the_content(); ?>
-    </main>
+            <a class="button button--outline" href="<?= wp_logout_url( $login_url ); ?>"><?= __( 'Logout', 'base-textdomain' ); ?></a>
+        </aside>
+        <main class="content-sidebar__content">
+            <?php the_content(); ?>
+        </main>
+    </div>
 </div>
 
 <?php get_footer(); ?>
