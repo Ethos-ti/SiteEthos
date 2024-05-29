58e8df7471b066fd91eafe83d47677919f6789f0
Everaldo Matias <matiaseveraldo@gmail.com>
2024-05-29 12:36:37 -0300

    Fix component content-sidebar on template-kitchen-sink.php

--- themes/hacklab-theme/template-kitchen-sink.php
+++ themes/hacklab-theme/template-kitchen-sink.php
@@ -337,21 +337,22 @@ get_header();
 
     <code>Container com sidebar</code>
     <div class="content-sidebar">
-        <aside class="content-sidebar__sidebar">
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-                <li>Item 4</li>
-            </ul>
-        </aside>
-        <main class="content-sidebar__content">
-            <h1>Título</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, felis eu pulvinar congue, ligula nunc congue quam, a tincidunt metus lacus quis tellus. Nulla facilisi. Praesent auctor, tortor sed consequat consequat, magna elit congue augue, eu vehicula nulla augue vel neque. Vivamus ut metus in metus bibendum tincidunt. Nullam euismod, felis sit amet tincidunt congue, augue odio scelerisque metus, a dignissim augue velit a risus. Sed vitae erat in purus condimentum congue. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent vel nibh et velit ultricies congue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, felis eu pulvinar congue, ligula nunc congue quam, a tincidunt metus lacus quis tellus. Nulla facilisi. Praesent auctor, tortor sed consequat consequat, magna elit congue augue, eu vehicula nulla augue vel neque. Vivamus ut metus in metus bibendum tincidunt. Nullam euismod, felis sit amet tincidunt congue, augue odio scelerisque metus, a dignissim augue velit a risus. Sed vitae erat in purus. </p>
-        </main>
+        <div class="container container--wide content-sidebar__container">
+            <aside class="content-sidebar__sidebar">
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                    <li>Item 4</li>
+                </ul>
+            </aside>
+            <main class="content-sidebar__content">
+                <h1>Título</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, felis eu pulvinar congue, ligula nunc congue quam, a tincidunt metus lacus quis tellus. Nulla facilisi. Praesent auctor, tortor sed consequat consequat, magna elit congue augue, eu vehicula nulla augue vel neque. Vivamus ut metus in metus bibendum tincidunt. Nullam euismod, felis sit amet tincidunt congue, augue odio scelerisque metus, a dignissim augue velit a risus. Sed vitae erat in purus condimentum congue. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent vel nibh et velit ultricies congue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, felis eu pulvinar congue, ligula nunc congue quam, a tincidunt metus lacus quis tellus. Nulla facilisi. Praesent auctor, tortor sed consequat consequat, magna elit congue augue, eu vehicula nulla augue vel neque. Vivamus ut metus in metus bibendum tincidunt. Nullam euismod, felis sit amet tincidunt congue, augue odio scelerisque metus, a dignissim augue velit a risus. Sed vitae erat in purus. </p>
+            </main>
+        </div>
     </div>
-
 </div>
 
 <?php get_footer(); ?>
