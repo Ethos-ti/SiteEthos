320fb674aaf9fd6b27ceda6c7f8892fc87419ed6
dioliviers <dioliviers@gmail.com>
2024-05-31 17:33:30 -0300

    inserido tags, leia mais e feito formulário para fazer download do livro

--- themes/hacklab-theme/single-publicacao.php
+++ themes/hacklab-theme/single-publicacao.php
@@ -1,17 +1,28 @@
 <?php
     get_header();
+    $categories = get_the_category();
 ?>
 <div class="container container--wide book-detail">
     <aside class="stack">
         <div class="thumbnail thumbnail--vertical">
             <?php the_post_thumbnail('card-large'); ?>
         </div>
-        <div class="btn"></div>
+        <div>
             <a class= "button button--outline" href="">Amostra da publicação</a>
-        </p>
+        </div>
     </aside>
     <main class="container container--wide">
+        <?php if (!empty($categories)): ?>
+            <div class="post-card__category">
+                <?php foreach ($categories as $category): ?>
+                    <a class="tag tag--solid tag--<?= $category->slug ?>" href="<?= get_term_link($category, 'category') ?>">
+                        <?= $category->name ?>
+                    </a>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
         <div class="post-header post-header__title">
+
             <h1><?php the_title() ?></h1>
         </div>
 
@@ -21,9 +32,36 @@
         </div>
         <div class="container">
             <p> <?php the_content() ?></p>
+
+            <div class="form-book">
+                <div class="form-book form-book__title">
+                    <div class="icon">icone</div>
+                    <h2>Solicite download gratuito</h2>
+                </div>
+                <div class="form-book--form-field">
+                    <label for="fname"><?php _e('name', 'hacklabr') ?></label>
+                    <input type="text" name="" class="text-input  form-book--input">
+                    <label for="fname"><?php _e('e-mail', 'hacklabr') ?></label>
+                    <input type="text" name="" class="text-input form-book--input">
+
+                    <label class="form-book__check">
+                        <input type="checkbox"><?php _e('I agree to receive newsletters from Instituto Ethos ', 'hacklabr') ?>
+                    </label>
+                    <label class="form-book__check">
+                        <input type="checkbox"><?php _e('I agree to be contacted by Instituto Ethos ', 'hacklabr') ?>
+                    </label>
+
+                    <div class="button form-book--book-button"><a class= "button button--outline" href="">enviar</a></div>
+                </div>
+            </div>
+
+
         </div>
 
     </main>
+
 </div>
+<?php get_template_part('template-parts/content/related-posts-book') ?>
+
 
 <?php get_footer() ?>
---
+++ themes/hacklab-theme/template-parts/content/related-posts-book.php
@@ -0,0 +1,47 @@
+<?php
+
+$post_id = get_the_ID();
+
+$projects = get_the_terms( $post_id, 'category' );
+
+if ( $projects && ! is_wp_error( $projects ) ) {
+    $projects = wp_list_pluck( $projects, 'term_id' );
+}
+
+$args = [
+    'post_type'      => 'publicacao',
+    'posts_per_page' => 3,
+    'post__not_in'   => [ $post_id ],
+    'orderby'        => 'date',
+    'order'          => 'DESC',
+    'tax_query'      => [
+        [
+            'taxonomy' => 'category',
+            'terms'    => $projects
+        ]
+    ],
+];
+
+$related_posts = new WP_Query( $args );
+
+if ( $related_posts->have_posts() ) : ?>
+
+<div class="related-posts container container--wide">
+    <h2 class="related-posts__title"><?php _e('Leia também: ') ?></h2>
+        <div class="related-posts__content">
+            <?php while( $related_posts->have_posts() ) :
+                $related_posts->the_post();
+                // Thumbnail
+                $thumbnail = ( has_post_thumbnail() ) ? get_the_post_thumbnail() : '<img src="' . get_stylesheet_directory_uri() . '/assets/images/default-image.png">'; ?>
+                 <?php get_template_part( 'template-parts/post-card', null, [
+                    'hide_author' => true,
+                    'hide_excerpt' => true,
+                    'hide_date' => true,
+                    'modifiers' => ['vertical-thumbnail']
+                ] ); ?>
+            <?php endwhile; ?>
+        </div>
+    </div>
+
+<?php endif;
+wp_reset_postdata(); ?>
