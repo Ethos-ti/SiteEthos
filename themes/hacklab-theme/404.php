91e7a5474dbe4b2877f4d36e45de3749a21da597
dioliviers <dioliviers@gmail.com>
2024-06-04 16:43:28 -0300

    efetuado ajustes de css na pagin 404

--- themes/hacklab-theme/404.php
+++ themes/hacklab-theme/404.php
@@ -1,12 +1,19 @@
-<?php get_header(); ?>
-<div class="error-404">
-        <h1>
-            <span class="error"><?php _e('erro', 'hacklabr') ?></span>
-            <span class="num">404</span>
-        </h1>
+<?php get_header();
 
-        <p><?php _e('Página não encontrada', 'hacklabr') ?></p>
-        <a href="<?= home_url() ?>" class="button"> <span><?php _e('Voltar para a página inicial', 'hacklabr') ?></span> </a>
+$home_url = '/home';
+$contact_url = '/contato/';
+?>
+<div class=" container container--narrow error-404">
+    <h1 class="post-header post-header__title">404</h1>
+    <span class="not-found">pagina não encontrada</span>
+    <div class="content">
+        <p>Parece que essa página não existe ou foi excluída, volte pra</p>
+        <span>home ou nos avise sobre o ocorrido</span>
     </div>
+    <div class="btn">
+        <a href="<?php echo $home_url; ?>" class="button button--outline">Ir para Home</a>
+        <a href="<?php echo $contact_url; ?>" class="button button--outline">Contato</a>
+    </div>
+</div>
 
 <?php get_footer(); ?>
