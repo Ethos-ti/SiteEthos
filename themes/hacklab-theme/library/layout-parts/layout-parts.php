da3bc9250c71b980b498e435f62ccb42c00b2720
Everaldo Matias <matiaseveraldo@gmail.com>
2024-06-05 09:25:03 -0300

    Improved rules to call templates

--- themes/hacklab-theme/library/layout-parts/layout-parts.php
+++ themes/hacklab-theme/library/layout-parts/layout-parts.php
@@ -19,6 +19,7 @@ add_action( 'template_redirect', 'hacklabr\\redirect_single_layout_archive' );
  * Get the layout part
  */
 function get_layout_part( $position ) {
+
     $stack = [$position];
 
     $queried_object = get_queried_object();
@@ -28,8 +29,11 @@ function get_layout_part( $position ) {
         $stack[] = "{$position}--archive";
 
         if ( is_home() ) {
+
             $stack[] = "{$position}--home";
+
         } else if ( is_author() ) {
+
             /**
              * @var \WP_User $queried_object
              */
@@ -39,6 +43,7 @@ function get_layout_part( $position ) {
             $stack[] = "{$position}--author-{$queried_object->user_nicename}";
 
         } else if ( is_category() || is_tag() || is_tax() ) {
+
             /**
              * @var \WP_Term $queried_object
              */
@@ -57,14 +62,16 @@ function get_layout_part( $position ) {
                 $stack[] = "{$position}--tag-{$queried_object->term_id}";
                 $stack[] = "{$position}--tag-{$queried_object->slug}";;
 
-            } else if( is_tax() ) {
+            } else if ( is_tax() ) {
 
                 $stack[] = "{$position}--taxonomy-{$queried_object->taxonomy}";
                 $stack[] = "{$position}--taxonomy-{$queried_object->taxonomy}-{$queried_object->term_id}";
                 $stack[] = "{$position}--taxonomy-{$queried_object->taxonomy}-{$queried_object->slug}";
 
             }
+
         } else if ( is_post_type_archive() ) {
+
             /**
              * @var \WP_Post_Type $queried_object
              */
@@ -72,10 +79,13 @@ function get_layout_part( $position ) {
             $stack[] = "{$position}--archive-{$queried_object->name}";
 
         } else if ( is_date() ) {
+
             $stack[] = "{$position}--date";
+
         }
 
-    }  else if ( is_front_page() ) {
+    } else if ( is_front_page() ) {
+
         /**
          * @var \WP_Post $queried_object
          */
@@ -85,26 +95,35 @@ function get_layout_part( $position ) {
         $stack[] = "{$position}--front-page-{$queried_object->post_name}";
 
     } else if ( is_single() ) {
+
         /**
          * @var \WP_Post $queried_object
          */
 
-        if ( is_singular( 'post' ) ) {
-            $stack[] = "{$position}--single";
-            $stack[] = "{$position}--single-{$queried_object->ID}";
-            $stack[] = "{$position}--single-{$queried_object->post_name}";
-        } else {
+        $stack[] = "{$position}--single";
+        $stack[] = "{$position}--single-{$queried_object->ID}";
+        $stack[] = "{$position}--single-{$queried_object->post_name}";
+
+        if ( ! is_singular( 'post' ) ) {
+
             $stack[] = "{$position}--single-{$queried_object->post_type}";
             $stack[] = "{$position}--single-{$queried_object->post_type}-{$queried_object->ID}";
             $stack[] = "{$position}--single-{$queried_object->post_type}-{$queried_object->post_name}";
+
         }
 
     } else if ( is_page() ) {
+
         /**
          * @var \WP_Post $queried_object
          */
 
+        $stack[] = "{$position}--page";
+        $stack[] = "{$position}--page-{$queried_object->ID}";
+        $stack[] = "{$position}--page-{$queried_object->post_name}";
+
         if ( is_page_template() ) {
+
             $template = get_page_template_slug();
             $template = str_replace( '.php', '', $template );
 
@@ -113,28 +132,28 @@ function get_layout_part( $position ) {
             $stack[] = "{$position}--page-template-{$template}-{$queried_object->ID}";
             $stack[] = "{$position}--page-template-{$template}-{$queried_object->post_name}";
 
-        } else {
-            $stack[] = "{$position}--page";
-            $stack[] = "{$position}--page-{$queried_object->ID}";
-            $stack[] = "{$position}--page-{$queried_object->post_name}";
         }
+
     } else if ( is_404() ) {
+
         $stack[] = "{$position}--404";
+
     } else if ( is_search() ) {
+
         /**
          * @var \WP_Query $wp_query
          */
 
         global $wp_query;
 
-        if ( $wp_query->post_count ) {
-            $stack[] = "{$position}--search";
-            $stack[] = "{$position}--search-{$wp_query->query_vars['s']}";
-        } else {
-            $stack[] = "{$position}--search";
+        $stack[] = "{$position}--search";
+        $stack[] = "{$position}--search-{$wp_query->query_vars['s']}";
+
+        if ( ! $wp_query->post_count ) {
             $stack[] = "{$position}--search-no-results";
             $stack[] = "{$position}--search-no-results-{$wp_query->query_vars['s']}";
         }
+
     }
 
     $stack = array_reverse( $stack );
@@ -147,6 +166,7 @@ function get_layout_part( $position ) {
     ] );
         
 	if ( $layout_part ) {
+
         /**
          * @var \WP_Post $layout_part
          */
@@ -155,13 +175,17 @@ function get_layout_part( $position ) {
         $html = '<div class="layout-part layout-part--' . $layout_part->post_name . ' layout-part--position-' . $position . '">';
 		$html .= apply_filters( 'the_content', $layout_part->post_content );
 		$html .=  '</div>';
+
 	} else {
+
         $html = '';
+
     }
 
 	wp_reset_postdata();
 
 	return $html;
+
 }
 
 function get_layout_part_header() {
