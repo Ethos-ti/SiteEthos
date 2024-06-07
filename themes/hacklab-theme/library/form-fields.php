6791f99f607d368b00572451064cf35a4384c526
Leonardo <leopiccionia@gmail.com>
2024-06-07 16:08:49 -0300

    Form styling (WIP)

--- themes/hacklab-theme/library/form-fields.php
+++ themes/hacklab-theme/library/form-fields.php
@@ -2,17 +2,19 @@
 
 namespace hacklabr\Fields;
 
-function render_input_field (string $name, mixed $value, array $definition) {
+function render_input_field (string $name, $value, array $definition) {
+    $placeholder = $definition['placeholder'] ?? '';
 ?>
     <input
+        class="form-field__input text-input"
         id="<?= $name ?>"
         name="_<?= $name ?>"
-        placeholder="<?= $definition['placeholder'] ?>"
+        placeholder="<?= $placeholder ?>"
         type="<?= $definition['type'] ?>"
         <?= $definition['required'] ? 'required' : '' ?>
         value="<?= $value ?>"
         aria-errormessage="<?= $name ?>__error"
-        <?= (isset($definition['disabled']) && $definition['disabled']) ? 'disabled' : '' ?>
+        <?= (!empty($definition['disabled'])) ? 'disabled' : '' ?>
     >
 <?php
 }
