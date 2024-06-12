77935759306c189b507f557b4d95d71a2f8da537
Thiago Monteiro <thiagomonteiro.geo@gmail.com>
2024-06-12 08:25:33 -0300

    estilo events-metadata

--- themes/hacklab-theme/single-evento.php
+++ themes/hacklab-theme/single-evento.php
@@ -6,42 +6,45 @@ $excerpt = !empty($post->post_excerpt) ? wp_kses_post($post->post_excerpt) : '';
 
 ?>
 
-<header class="post-header container container--wide">
+<header class="post-header">
+    <div class="post-header__postdata container--wide">
+        <h1 class="post-header__title"> <?php the_title(); ?> </h1>
 
-    <h1 class="post-header__title"> <?php the_title(); ?> </h1>
+        <?php if ( $excerpt ) : ?>
+            <p class="post-header__excerpt container--narrow"><?= get_the_excerpt() ?></p>
+        <?php endif; ?>
 
-    <?php if ( $excerpt ) : ?>
-        <p class="post-header__excerpt container--narrow"><?= get_the_excerpt() ?></p>
-    <?php endif; ?>
-
-    <div class="post-header__meta container">
-        <p class="post-header__date"><?php _e('Published in ', 'hacklabr') ?><?= get_the_date() ?></p>
-        <?php get_template_part('template-parts/share-links', null, ['link' => get_the_permalink()]) ?>
+        <div class="post-header__meta container">
+            <p class="post-header__date"><?php _e('Published in ', 'hacklabr') ?><?= get_the_date() ?></p>
+            <?php get_template_part('template-parts/share-links', null, ['link' => get_the_permalink()]) ?>
+        </div>
     </div>
 
-    <div class="event-metadada-container">
-        <div class="event-metadada__infos">
-            <div class="event-metadada__infos__date">
-                <p><?php _e('Date', 'Hacklabr') ?></p>
-                <p><?php _e('08/10/2024', 'Hacklabr') ?></p>
+    <div class="event-metadada-section">
+        <div class="event-metadada container">
+            <div class="event-metadada__infos">
+                <div class="event-metadada__infos__date">
+                    <p><?php _e('Date', 'Hacklabr') ?></p>
+                    <p><?php _e('08/10/2024', 'Hacklabr') ?></p>
+                </div>
+                <div class="event-metadada__infos__time">
+                    <p><?php _e('Time', 'Hacklabr') ?></p>
+                    <p><?php _e('das 8h às 18h30', 'Hacklabr') ?></p>
+                </div>
+                <div class="event-metadada__infos__workload">
+                    <p><?php _e('Workload', 'Hacklabr') ?></p>
+                    <p><?php _e('3 horas', 'Hacklabr') ?></p>
+                </div>
+                <div class="event-metadada__infos__investment">
+                    <p><?php _e('Investment', 'Hacklabr') ?></p>
+                    <p><?php _e('459,00 (50% de desconto para estudantes)', 'Hacklabr') ?></p>
+                </div>
             </div>
-            <div class="event-metadada__infos__time">
-                <p><?php _e('Time', 'Hacklabr') ?></p>
-                <p><?php _e('das 8h às 18h30', 'Hacklabr') ?></p>
-            </div>
-            <div class="event-metadada__infos__workload">
-                <p><?php _e('Workload', 'Hacklabr') ?></p>
-                <p><?php _e('3 horas', 'Hacklabr') ?></p>
-            </div>
-            <div class="event-metadada__infos__investment">
-                <p><?php _e('Investment', 'Hacklabr') ?></p>
-                <p><?php _e('459,00 (50% de desconto para estudantes)', 'Hacklabr') ?></p>
-            </div>
-        </div>
-        <div class="event-metadada__location">
-            <div class="event-metadada__location__local">
-                <p><?php _e('Local', 'Hacklabr') ?></p>
-                <p><?php _e('Online e ao vivo, via webconferência nas plataformas Zoom e Meeting', 'Hacklabr') ?></p>
+            <div class="event-metadada__location">
+                <div class="event-metadada__location__local">
+                    <p><?php _e('Local', 'Hacklabr') ?></p>
+                    <p><?php _e('Online e ao vivo, via webconferência nas plataformas Zoom e Meeting', 'Hacklabr') ?></p>
+                </div>
             </div>
         </div>
     </div>
