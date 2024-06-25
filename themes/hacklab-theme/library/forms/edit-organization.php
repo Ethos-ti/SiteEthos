deec6a161a5aebaddcf0d57dabe5e4641fc5287f
Leonardo <leopiccionia@gmail.com>
2024-06-25 10:41:03 -0300

    Add hidden input to edit organization form

--- themes/hacklab-theme/library/forms/edit-organization.php
+++ themes/hacklab-theme/library/forms/edit-organization.php
@@ -5,20 +5,35 @@ namespace hacklabr;
 function register_edit_organization_form () {
     $fields = get_registration_step1_fields();
 
+    unset($fields['termos_de_uso']);
+
     register_form('edit-organization', __('Edit organization', 'hacklabr'), [
         'fields' => $fields,
         'submit_label' => __('Edit', 'hacklabr'),
         'get_params' => function () use ($fields) {
             $user_id = get_current_user_id();
-            $organizations = get_posts([
 
+            $organizations = get_posts([
                 'post_type' => 'organizacao',
                 'author' => $user_id,
                 'posts_per_page' => 1,
             ]);
 
-            if(!empty($organizations)) {
+            if (!empty($organizations)) {
                 $organization = $organizations[0];
+
+                add_action('hacklabr\\form_output', function ($form_html, $form) use($organization) {
+                    if ($form['id'] !== 'edit-organization') {
+                        return $form_html;
+                    }
+
+                    $form_lines = explode("\n", $form_html);
+                    array_splice($form_lines, 1, 0, [
+                        '<input type="hidden" name="__organization_id" value="' . $organization->ID . '">',
+                    ]);
+                    return implode("\n", $form_lines);
+                });
+
                 $meta = get_post_meta($organization->ID);
                 $params = [];
 
@@ -30,6 +45,7 @@ function register_edit_organization_form () {
 
                 return $params;
             }
+
             return [];
         },
     ]);
