f4b23c1c75927ccb423ac856a02aad82d04a591b
dioliviers <dioliviers@gmail.com>
2024-06-26 11:04:23 -0300

    retirado a regra do header_title para não exibir na pagina de formulario enviado

--- themes/hacklab-theme/page.php
+++ themes/hacklab-theme/page.php
@@ -4,11 +4,13 @@ $excerpt = !empty( $post->post_excerpt ) ? wp_kses_post( $post->post_excerpt ) :
 ?>
     <div class="container">
         <div class="post-header">
+        <?php if ( !is_page('formulario-enviado') ) : ?>
             <h1 class="post-header__title alignwide"><?php the_title() ?></h1>
             <?php if( $excerpt ) : ?>
                 <p class="post-header__excerpt container container--normal"><?= get_the_excerpt() ?></p>
             <?php endif; ?>
-        </div>
+        <?php endif; ?>
+    </div>
         <div class="post-content content stack">
             <?php the_content() ?>
         </div>
