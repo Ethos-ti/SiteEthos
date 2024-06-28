f85ab2c86ea42de56ea2b5c411d26f313eda926f
Leonardo <leopiccionia@gmail.com>
2024-06-28 14:05:12 -0300

    Remove transaction token from finished registrations

--- themes/hacklab-theme/library/cron.php
+++ themes/hacklab-theme/library/cron.php
@@ -13,6 +13,8 @@ function add_cron_schedules (array $schedules) {
 add_filter('cron_schedules', 'hacklabr\\add_cron_schedules');
 
 function clean_abandoned_registrations () {
+    $TRANSACTION_KEY = '_ethos_transaction';
+
     $abandoned_posts = get_posts([
         'post_type' => ['organizacao'],
         'post_status' => ['draft', 'ethos_under_progress'],
@@ -36,6 +38,37 @@ function clean_abandoned_registrations () {
 
     foreach ($abandoned_users as $abandoned_user) {
         wp_delete_user($abandoned_user->ID, null);
+    }
+
+    $finished_posts = get_posts([
+        'post_type' => ['organizacao'],
+        'post_status' => ['publish'],
+        'date_query' => [
+            [ 'before' => '1 day ago', 'inclusive' => true ],
+        ],
+        'meta_query' => [
+            [ 'key' => $TRANSACTION_KEY, 'compare' => 'EXISTS' ],
+        ],
+        'posts_per_page' => 20,
+    ]);
+
+    foreach ($finished_posts as $finished_post) {
+        delete_post_meta($finished_post->ID, $TRANSACTION_KEY);
+    }
+
+    $finished_users = get_users([
+        'role__in' => ['subscriber'],
+        'date_query' => [
+            [ 'before' => '1 day ago', 'inclusive' => true ],
+        ],
+        'meta_query' => [
+            [ 'key' => $TRANSACTION_KEY, 'compare' => 'EXISTS' ],
+        ],
+        'number' => 20,
+    ]);
+
+    foreach ($finished_users as $finished_user) {
+        delete_user_meta($finished_user->ID, $TRANSACTION_KEY);
     }
 }
 add_action('hacklabr\\run_every_hour', 'hacklabr\\clean_abandoned_registrations');
