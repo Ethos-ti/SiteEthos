6758688dc8ac84a97efd29be1005ec504ac581c8
dioliviers <dioliviers@gmail.com>
2024-07-03 12:02:28 -0300

    inserido parametro get_the_id em the_title

--- themes/hacklab-theme/template-parts/title/blog.php
+++ themes/hacklab-theme/template-parts/title/blog.php
@@ -1,7 +1,7 @@
 <header class="c-title title-blog">
     <div class="container">
         <h1 class="entry-title">
-            <?php echo apply_filters( 'the_title' , get_the_title( get_option( 'page_for_posts' ) ) ); ?>
+            <?php echo apply_filters( 'the_title' , get_the_title( get_option( 'page_for_posts' ) ), get_the_ID() ); ?>
         </h1>
     </div>
 </header><!-- /.c-title.title-blog -->
