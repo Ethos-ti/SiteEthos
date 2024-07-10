50d89817cbce8ad44e26b4e2e03fe1b2a2348ddb
Filipe de Souza <filipe.souza@hacklab.com.br>
2024-07-10 18:26:32 -0300

    apply invoice page and link pages #40

--- themes/hacklab-theme/library/blocks/payments-history/payments-history.php
+++ themes/hacklab-theme/library/blocks/payments-history/payments-history.php
@@ -7,7 +7,9 @@ function get_payments_history_data ($attributes): array {
 }
 
 function render_payments_history_callback ($attributes) {
-    $query = get_posts_grid_data($attributes);
+    $query = get_payments_history_data($attributes);
+
+    $invoice_page = get_permalink(get_page_by_template('template-invoice.php'));
 
     ob_start();
     ?>
@@ -16,7 +18,7 @@ function render_payments_history_callback ($attributes) {
       <thead>
         <tr>
             <th>Data</th>
-            <th class="id-pedido">Id do Pedido</th>
+            <th class="id__pedido">Id do Pedido</th>
             <th>Nível</th>
             <th>Valor</th>
             <th>Status</th>
@@ -26,42 +28,60 @@ function render_payments_history_callback ($attributes) {
       <tbody>
       <tr>
         <td>Janeiro 2024</td>
-        <td class="id-pedido">1F5E4D4784</td>
+        <?php
+            $invoice_link = add_query_arg(['pedido'=> '1F5E4D4784'], $invoice_page);
+        ?>
+        <td class="id__pedido"><a href="<?= $invoice_link ?>">1F5E4D4784</a></td>
         <td>Plano Vivência</td>
         <td>R$810,00</td>
         <td class="status status--pendente"><span>Pendente</span></td>
       </tr>
       <tr>
         <td>Dezembro 2023</td>
-        <td class="id-pedido">5BEE44D535</td>
+        <?php
+            $invoice_link = add_query_arg(['pedido'=> '5BEE44D535'], $invoice_page);
+        ?>
+        <td class="id__pedido"><a href="<?= $invoice_link ?>">5BEE44D535</a></td>
         <td>Plano Vivência</td>
         <td>R$810,00</td>
         <td class="status status--pendente"><span>Pendente</span></td>
       </tr>
       <tr>
         <td>Novembro 2023</td>
-        <td class="id-pedido">5097EECC89</td>
+        <?php
+            $invoice_link = add_query_arg(['pedido'=> '5097EECC89'], $invoice_page);
+        ?>
+        <td class="id__pedido"><a href="<?= $invoice_link ?>">5097EECC89</a></td>
         <td>Plano Vivência</td>
         <td>R$810,00</td>
         <td class="status status--pago"><span>Pago</span></td>
       </tr>
       <tr>
         <td>Outubro 2023</td>
-        <td class="id-pedido">564S81B7C6</td>
+        <?php
+            $invoice_link = add_query_arg(['pedido'=> '564S81B7C6'], $invoice_page);
+        ?>
+        <td class="id__pedido"><a href="<?= $invoice_link ?>">564S81B7C6</a></td>
         <td>Plano Vivência</td>
         <td>R$810,00</td>
         <td class="status status--pago"><span>Pago</span></td>
       </tr>
       <tr>
         <td>Setembro 2023</td>
-        <td class="id-pedido">99F4653F21</td>
+        <?php
+            $invoice_link = add_query_arg(['pedido'=> '99F4653F21'], $invoice_page);
+        ?>
+        <td class="id__pedido"><a href="<?= $invoice_link ?>">99F4653F21</a></td>
         <td>Plano Vivência</td>
         <td>R$810,00</td>
         <td class="status status--pago"><span>Pago</span></td>
       </tr>
       <tr>
         <td>Agosto 2023</td>
-        <td class="id-pedido">2DFFBC4B8D</td>
+        <?php
+            $invoice_link = add_query_arg(['pedido'=> '2DFFBC4B8D'], $invoice_page);
+        ?>
+        <td class="id__pedido"><a href="<?= $invoice_link ?>">2DFFBC4B8D</a></td>
         <td>Plano Vivência</td>
         <td>R$810,00</td>
         <td class="status status--pago" ><span>Pago</span></td>
