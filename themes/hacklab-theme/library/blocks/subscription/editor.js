c155afc092ff47ea26f0d20f6879eff640d10d9d
Filipe de Souza <filipe.souza@hacklab.com.br>
2024-07-10 17:06:56 -0300

    create block for subscription #38

---
+++ themes/hacklab-theme/library/blocks/subscription/editor.js
@@ -0,0 +1,20 @@
+import { useBlockProps } from '@wordpress/block-editor';
+import { registerBlockType } from '@wordpress/blocks';
+import { Placeholder } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+import metadata from './block.json';
+
+function Edit ({ attributes, setAttributes }) {
+    const blockProps = useBlockProps();
+
+    return (
+        <div {...blockProps}>
+            <Placeholder label={__('Subscription', 'hacklabr')} icon="money-alt"/>
+        </div>
+    );
+}
+
+registerBlockType(metadata.name, {
+    edit: Edit,
+});
---
+++ themes/hacklab-theme/library/blocks/subscription/subscription.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace hacklabr;
+
+function get_subscription_data ($attributes): array {
+    return [];
+}
+
+function render_subscription_callback ($attributes) {
+    $query = get_posts_grid_data($attributes);
+
+    ob_start();
+    ?>
+
+    <table class="hacklabr-subscription-block">
+      <thead>
+        <tr>
+            <th>Data</th>
+            <th>Título</th>
+            <th>Tipo de inscrição</th>
+            <th>Status</th>
+        </tr>
+      </thead>
+
+      <tbody>
+      <tr>
+        <td>Janeiro 2024</td>
+        <td><a href="#">Valor Estratégico/ODS e Agenda 2030</a></td>
+        <td>Curso</td>
+        <td class="status status--pendente"><span>Pendente</span></td>
+      </tr>
+      <tr>
+        <td>Dezembro 2023</td>
+        <td><a href="#">Título da palestra lorem ipsum</a></td>
+        <td>Palestra</td>
+        <td class="status status--pendente"><span>Pendente</span></td>
+      </tr>
+      <tr>
+        <td>Novembro 2023</td>
+        <td><a href="#">Título da palestra lorem ipsum</a></td>
+        <td>Curso</td>
+        <td class="status status--inscrito"><span>Inscrito</span></td>
+      </tr>
+      <tr>
+        <td>Outubro 2023</td>
+        <td><a href="#">Título da palestra lorem ipsum</a></td>
+        <td>Palestra</td>
+        <td class="status status--inscrito"><span>Inscrito</span></td>
+      </tr>
+      <tr>
+        <td>Setembro 2023</td>
+        <td><a href="#">Título da palestra lorem ipsum</a></td>
+        <td>Curso</td>
+        <td class="status status--inscrito"><span>Inscrito</span></td>
+      </tr>
+      <tr>
+        <td>Agosto 2023</td>
+        <td><a href="#">Título da palestra lorem ipsum</a></td>
+        <td>Palestra</td>
+        <td class="status status--inscrito" ><span>Inscrito</span></td>
+      </tr>
+      </tbody>
+    </table>
+
+    <?php
+    $output = ob_get_clean();
+
+    return $output;
+}
