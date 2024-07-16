9b71b1f1953d0dd7d9169c28b7332c3a8c42d637
leopiccionia <leopiccionia@gmail.com>
2024-07-16 09:30:22 -0300

    Don't delete abandoned registrations anymore

--- themes/hacklab-theme/library/cron.php
+++ themes/hacklab-theme/library/cron.php
@@ -12,40 +12,12 @@ function add_cron_schedules (array $schedules) {
 }
 add_filter('cron_schedules', 'hacklabr\\add_cron_schedules');
 
-function clean_abandoned_registrations () {
-    // Required for using `wp_delete_user` function
-	require_once(ABSPATH . 'wp-admin/includes/user.php');
-
+function clean_transactions () {
     $TRANSACTION_KEY = '_ethos_transaction';
 
-    $abandoned_posts = get_posts([
+    $posts = get_posts([
         'post_type' => ['organizacao'],
-        'post_status' => ['draft', 'ethos_under_progress'],
-        'date_query' => [
-            [ 'before' => '1 day ago', 'inclusive' => true ],
-        ],
-        'posts_per_page' => 20,
-    ]);
-
-    foreach ($abandoned_posts as $abandoned_post) {
-        wp_delete_post($abandoned_post->ID, true);
-    }
-
-    $abandoned_users = get_users([
-        'role__in' => ['ethos_under_progress'],
-        'date_query' => [
-            [ 'before' => '1 day ago', 'inclusive' => true ],
-        ],
-        'number' => 20,
-    ]);
-
-    foreach ($abandoned_users as $abandoned_user) {
-        wp_delete_user($abandoned_user->ID, null);
-    }
-
-    $finished_posts = get_posts([
-        'post_type' => ['organizacao'],
-        'post_status' => ['publish'],
+        'post_status' => ['draft', 'ethos_under_progress', 'publish'],
         'date_query' => [
             [ 'before' => '1 day ago', 'inclusive' => true ],
         ],
@@ -55,12 +27,12 @@ function clean_abandoned_registrations () {
         'posts_per_page' => 20,
     ]);
 
-    foreach ($finished_posts as $finished_post) {
-        delete_post_meta($finished_post->ID, $TRANSACTION_KEY);
+    foreach ($posts as $post) {
+        delete_post_meta($post->ID, $TRANSACTION_KEY);
     }
 
-    $finished_users = get_users([
-        'role__in' => ['subscriber'],
+    $users = get_users([
+        'role__in' => ['ethos_under_progress', 'subscriber'],
         'date_query' => [
             [ 'before' => '1 day ago', 'inclusive' => true ],
         ],
@@ -70,11 +42,11 @@ function clean_abandoned_registrations () {
         'number' => 20,
     ]);
 
-    foreach ($finished_users as $finished_user) {
-        delete_user_meta($finished_user->ID, $TRANSACTION_KEY);
+    foreach ($users as $user) {
+        delete_user_meta($user->ID, $TRANSACTION_KEY);
     }
 }
-add_action('hacklabr\\run_every_hour', 'hacklabr\\clean_abandoned_registrations');
+add_action('hacklabr\\run_every_hour', 'hacklabr\\clean_transactions');
 
 function schedule_recurring_tasks () {
     wp_unschedule_hook('hacklabr\\run_every_hour');
