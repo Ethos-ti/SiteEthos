9879abaefb965be9c2bc701fd1004e2cedf8ecc0
leopiccionia <leopiccionia@gmail.com>
2024-07-30 16:15:39 -0300

    Improve interval sent to shortcode

--- themes/hacklab-theme/library/blocks/talk-request/talk-request.php
+++ themes/hacklab-theme/library/blocks/talk-request/talk-request.php
@@ -3,12 +3,15 @@
 namespace hacklabr;
 
 function render_talk_request_callback ($attributes) {
-    $start_date = date( 'd/m/Y', strtotime( '+30 days' ) );
+    $start = strtotime('2021-01-01');
+    $end = strtotime('+2 years');
+
+    $days = ceil(($end - $start) / DAY_IN_SECONDS) + (2 * 365);
 
     ob_start();
 ?>
     <div class="hacklabr-talk-request-block">
-        <?= do_shortcode('[ethosListaPalestras tp="16" sd="' . $start_date .' " days="700"]') ?>
+        <?= do_shortcode('[ethosListaPalestras tp="16" sd="01/01/2021" days="' . $days . '"]') ?>
     </div>
 <?php
     $output = ob_get_clean();
