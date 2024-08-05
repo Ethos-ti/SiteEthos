1d96d8b049c5a4af8b77f5779e4b9d419197c99b
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2024-08-04 22:06:17 -0300

    adiciona redirecionamentos para as urls dos eventos

--- themes/hacklab-theme/functions.php
+++ themes/hacklab-theme/functions.php
@@ -4,6 +4,8 @@ namespace hacklabr;
 
 load_theme_textdomain('hacklabr');
 
+require __DIR__ . '/library/ethos-associados-redirects.php';
+
 require __DIR__ . '/library/layout-parts/layout-parts.php';
 require __DIR__ . '/library/supports.php';
 require __DIR__ . '/library/sidebars.php';
