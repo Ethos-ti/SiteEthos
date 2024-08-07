d6409fc1011851b9f3ebbe2d0f9aa51bb83cc578
Jacson Passold <jacson@passold.com.br>
2024-08-07 14:59:51 -0300

    redirect pagamento

--- themes/hacklab-theme/library/ethos-associados-redirects.php
+++ themes/hacklab-theme/library/ethos-associados-redirects.php
@@ -1,12 +1,15 @@
 <?php
 add_action('init', function (){
-    if(str_starts_with($_SERVER['REQUEST_URI'], '/conteudo/inscricao-evento')) {
+    if(
+        str_starts_with($_SERVER['REQUEST_URI'], '/conteudo/inscricao-evento') ||
+        str_starts_with($_SERVER['REQUEST_URI'], '/conteudo/inscricao-conferencia')
+     ) {
         global $wpdb;
         if(isset($_GET['id']) && is_numeric($_GET['id'])) {
             $event_id = (int) $_GET['id'];
-            
+
             $sql = "SELECT post_id FROM $wpdb->postmeta WHERE meta_key = '_ethos_crm:fut_pf_id' AND meta_value = %d";
-            
+
             // procura no banco de dados o post pelo id do evento que está no metadado _ethos_crm:fut_pf_id
             if($post_id = $wpdb->get_var($wpdb->prepare($sql, $event_id))){
                 unset($_GET['id']);
@@ -16,7 +19,7 @@ add_action('init', function (){
                 wp_redirect(get_permalink($post_id) . "{$query_vars}", 301);
                 die;
             }
-    
+
         }
     }
 
@@ -24,15 +27,23 @@ add_action('init', function (){
         global $wpdb;
         if(isset($_GET['id']) && is_numeric($_GET['id'])) {
             $event_id = (int) $_GET['id'];
-            
+
             $sql = "SELECT post_id FROM $wpdb->postmeta WHERE meta_key = '_ethos_crm:fut_pf_id' AND meta_value = %d";
-            
+
             // procura no banco de dados o post pelo id do evento que está no metadado _ethos_crm:fut_pf_id
             if($post_id = $wpdb->get_var($wpdb->prepare($sql, $event_id))){
                 wp_redirect(get_permalink($post_id) . "?certificado", 301);
                 die;
             }
-    
+
         }
+    }
+
+    if(str_starts_with($_SERVER['REQUEST_URI'], '/conteudo/acesso-a-pagamento')) {
+        if($query_vars = http_build_query($_GET)){
+            $query_vars = "?{$query_vars}";
+        }
+        wp_redirect( '/acesso-a-pagamento'. "{$query_vars}", 301 );
+        die;
     }
 });
