5ebf9f7ce33fb49fcdff89de8c22d356fbda1ffb
dioliviers <dioliviers@gmail.com>
2024-08-08 17:26:32 -0300

    ajustes finos na archive agenda

--- themes/hacklab-theme/template-parts/partners-list.php
+++ themes/hacklab-theme/template-parts/partners-list.php
@@ -1,7 +1,7 @@
-<?php
-if ( is_post_type_archive( 'tribe_events' ) || ( is_singular() && get_post_meta( get_the_ID(), 'exibir_parceiros', true ) === 'Sim, exibir parceiros' ) ) :
-?>
-    <div class="partners-list container container--wide">
-        <?php echo hacklabr\get_layout_part_footer(); ?>
-    </div>
-<?php endif; ?>
+    <?php
+    if ( is_post_type_archive( 'tribe_events' ) || ( is_singular() && get_post_meta( get_the_ID(), 'exibir_parceiros', true ) === 'Sim, exibir parceiros' ) ) :
+    ?>
+        <div class="partners-list container container--wide">
+            <?php echo hacklabr\get_layout_part_footer(); ?>
+        </div>
+    <?php endif; ?>
