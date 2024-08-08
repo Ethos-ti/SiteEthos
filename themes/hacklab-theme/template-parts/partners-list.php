2894c8d9657717539736625a0056fc37709c8538
dioliviers <dioliviers@gmail.com>
2024-08-08 17:17:47 -0300

    ajuste nos parceiros na agenda

--- themes/hacklab-theme/template-parts/partners-list.php
+++ themes/hacklab-theme/template-parts/partners-list.php
@@ -1,8 +1,5 @@
 <?php
-if (
-    ( is_single() || is_page() || is_front_page() ) && get_post_meta( get_the_ID(), 'exibir_parceiros', true ) === 'Sim, exibir parceiros'
-    || is_post_type_archive('tribe_events')
-) :
+if ( is_post_type_archive( 'tribe_events' ) || ( is_singular() && get_post_meta( get_the_ID(), 'exibir_parceiros', true ) === 'Sim, exibir parceiros' ) ) :
 ?>
     <div class="partners-list container container--wide">
         <?php echo hacklabr\get_layout_part_footer(); ?>
