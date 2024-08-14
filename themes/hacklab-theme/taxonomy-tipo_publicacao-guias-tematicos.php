2f10d0d0e7db3e09feab4efacd721888361d78f7
Filipe de Souza <filipe.souza@hacklab.com.br>
2024-08-14 10:56:39 -0300

    fix bug in tab all (#149)

--- themes/hacklab-theme/taxonomy-tipo_publicacao-guias-tematicos.php
+++ themes/hacklab-theme/taxonomy-tipo_publicacao-guias-tematicos.php
@@ -5,67 +5,64 @@ global $wp_query;
 
 $post_type = get_post_type() ?: 'publicacao';
 
-// Obter o termo atual da taxonomia 'tipo_post'
 $tipo_post_term = get_queried_object();
 
 $tipo_post_slug = $tipo_post_term->slug;
 
-// Obter termos da taxonomia 'category' e 'tipo_post'
-$terms = get_terms_by_use_menu('category', $post_type);
+$terms = get_terms_by_use_menu( 'category', $post_type );
 
-if($wp_query->get('category')){
-    $active_tab = $wp_query->get('category');
+if ( $wp_query->get( 'category' ) ) {
+    $active_tab = $wp_query->get( 'category' );
 } else {
     $active_tab = 'all';
 }
 
-$desired_order = [ 'DIREITOS HUMANOS', 'INTEGRIDADE', 'GESTÃO SUSTENTÁVEL', 'MEIO AMBIENTE', 'INSTITUCIONAL' ];
+$desired_order = ['DIREITOS HUMANOS', 'INTEGRIDADE', 'GESTÃO SUSTENTÁVEL', 'MEIO AMBIENTE', 'INSTITUCIONAL'];
 
-function custom_sort($a, $b) {
+function custom_sort( $a, $b ) {
     global $desired_order;
-    $pos_a = array_search($a->name, $desired_order);
-    $pos_b = array_search($b->name, $desired_order);
+    $pos_a = array_search( $a->name, $desired_order );
+    $pos_b = array_search( $b->name, $desired_order );
 
-    if ($pos_a === false && $pos_b === false) {
+    if ( $pos_a === false && $pos_b === false ) {
         return 0;
-    } elseif ($pos_a === false) {
+    } elseif ( $pos_a === false ) {
         return 1;
-    } elseif ($pos_b === false) {
+    } elseif ( $pos_b === false ) {
         return -1;
     } else {
         return $pos_a - $pos_b;
     }
 }
 
-usort($terms, 'custom_sort');
+usort( $terms, 'custom_sort' );
 
 ?>
 <div class="container container--wide">
 
     <?php echo hacklabr\get_layout_part_header(); ?>
 
-    <?php if ( $terms && ! is_wp_error( $terms ) ) : ?>
-        <div class="tabs" x-data="{ currentTab: '<?php echo $active_tab?>' }" x-bind="Tabs($data)">
+    <?php if ( $terms && !is_wp_error( $terms ) ) : ?>
+        <div class="tabs" x-data="{ currentTab: '<?php echo $active_tab ?>' }" x-bind="Tabs( $data )">
             <div class="tabs__header" role="tablist">
-                <a class="tab" x-bind="TabButton('all', $data)" href="<?= esc_url(add_query_arg(array( 'tipo_publicacao' => $tipo_post_slug), get_post_type_archive_link('tipo_publicacao'))); ?>"><?php _e('All the themes', 'hacklabr') ?></a>
+                <a class="tab" x-bind="TabButton('all', $data)" href="<?= esc_url( add_query_arg ( array ( 'tipo_publicacao' => $tipo_post_slug ), get_post_type_archive_link( 'publicacao' ) ) ); ?>"><?php _e( 'All the themes', 'hacklabr' ) ?></a>
                 <?php foreach ( $terms as $term ) : ?>
-                    <a class="tab" x-bind="TabButton('<?= esc_attr($term->slug); ?>', $data)" href="<?= esc_url(add_query_arg(array('tipo_publicacao' => $tipo_post_slug, 'category' => $term->slug), get_post_type_archive_link('tipo_publicacao'))); ?>">
-                        <?= esc_attr($term->name); ?>
+                    <a class="tab" x-bind="TabButton( '<?= esc_attr( $term->slug ); ?>', $data )" href="<?= esc_url( add_query_arg( array( 'tipo_publicacao' => $tipo_post_slug, 'category' => $term->slug ), get_post_type_archive_link( 'publicacao' ) ) ); ?>">
+                        <?= esc_attr( $term->name ); ?>
                     </a>
                 <?php endforeach; ?>
             </div>
             <div class="tabs__panels">
-                <div class="tabs__panel" x-bind="TabPanel('<?php echo $active_tab?>', $data)">
-                <main class="posts-grid__content">
+                <div class="tabs__panel" x-bind="TabPanel( '<?php echo $active_tab ?>', $data )">
+                    <main class="posts-grid__content">
                         <?php
                         // Verificação dos parâmetros
-                        $current_category = get_query_var('category');
-                        $current_tipo_post = get_query_var('tipo_publicacao');
+                        $current_category = get_query_var( 'category' );
+                        $current_tipo_post = get_query_var( 'tipo_publicacao' );
 
                         $args = array(
                             'post_type' => 'publicacao',
                             'tax_query' => array(
-                                'relation' => 'AND',
                                 array(
                                     'taxonomy' => 'tipo_publicacao',
                                     'field' => 'slug',
@@ -74,8 +71,7 @@ usort($terms, 'custom_sort');
                             ),
                         );
 
-                        // Adicionar filtro de categoria se estiver presente
-                        if ($current_category) {
+                        if ( $current_category && $current_category !== 'all' ) {
                             $args['tax_query'][] = array(
                                 'taxonomy' => 'category',
                                 'field' => 'slug',
@@ -85,14 +81,14 @@ usort($terms, 'custom_sort');
 
                         $query = new WP_Query($args);
 
-                        if ($query->have_posts()) :
-                            while ($query->have_posts()) : $query->the_post();
-                                get_template_part( 'template-parts/post-card', null, [
+                        if ( $query->have_posts() ) :
+                            while ( $query->have_posts() ) : $query->the_post();
+                                get_template_part('template-parts/post-card', null, [
                                     'hide_author' => true,
                                     'hide_excerpt' => true,
                                     'modifiers' => ['vertical-thumbnail'],
                                     'show_taxonomies' => ['tipo_publicacao'],
-                                ] );
+                                ]);
                             endwhile;
                             wp_reset_postdata();
 
