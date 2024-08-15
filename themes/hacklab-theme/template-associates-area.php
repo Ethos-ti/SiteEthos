8c4706d38b2cfe16a2f99bb37d76e565dc1e2203
Filipe de Souza <filipe.souza@hacklab.com.br>
2024-08-15 13:59:09 -0300

    create success page in solicitacoes (#139)

--- themes/hacklab-theme/template-associates-area.php
+++ themes/hacklab-theme/template-associates-area.php
@@ -21,7 +21,7 @@ $current_post_id = get_the_ID();
             if ( $associates_areas ) {
                 echo '<ul class="content-sidebar__list">';
                 foreach ( $associates_areas as $associates_area ) {
-                    if(show_associated_page($associates_area)){
+                    if ( show_associated_page( $associates_area ) && $associates_area->post_name !== 'solicitacao-enviada' ) {
                         $css_class = $current_post_id === $associates_area->ID ? 'content-sidebar__list-item content-sidebar__list-item--active' : 'content-sidebar__list-item';
                         echo '<li class="' . $css_class . '"><a href="' . esc_url( get_permalink( $associates_area ) ) . '">' . wp_kses_post( get_the_title( $associates_area ) ) . '</a></li>';
                     }
